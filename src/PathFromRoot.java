3c3d4dd08e5dc2ea2890dead57c56398054d6dd5
shaha <fshachar@campus.technion.ac.il>
2023-06-30 01:14:36 +0300

    finished part A question 1 (test worked)

--- src/PathFromRoot.java
+++ src/PathFromRoot.java
@@ -1,6 +1,19 @@
 public class PathFromRoot {
     public static boolean doesPathExist(BinNode<Character> root, String str) {
-        // TODO: Add your code for part A1 here...
+        //end of string
+        if(str.isEmpty())
+            return true;
+        //reached end of branch before finishing path
+        if(root == null)
+            return false;
+        //the current node's letter doesn't continue path
+        if(root.getData() != str.charAt(0))
+            return false;
+
+        //checking if path for str minus the first word exists in the right or left sub trees
+        boolean is_right_path = doesPathExist(root.getRight(), str.substring(1));
+        boolean is_left_path = doesPathExist(root.getLeft(), str.substring(1));
+        return (is_right_path || is_left_path);
     }
 
 }
