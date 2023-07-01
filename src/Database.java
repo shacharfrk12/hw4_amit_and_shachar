6624807fb7bc6088dc50fdd8b9d7ac36c2277008
shaha <fshachar@campus.technion.ac.il>
2023-07-01 16:47:09 +0300

    almost finished

--- src/Database.java
+++ src/Database.java
@@ -1,5 +1,7 @@
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.Condition;
@@ -8,13 +10,22 @@ import java.util.concurrent.locks.Condition;
 public class Database {
     private Map<String, String> data;
     private int numOfReaders;
+    private Set<Long> pidReaders;
+    private Long pidWriter;
+    private boolean isSomeoneWriting;
     private final int MAX_READERS;
-    private Lock accessNumReaderLock;
+    private Lock accessTInfoLock;
+    private Condition canReadWrite;
 
     public Database(int maxNumOfReaders) {
         data = new HashMap<>();  // Note: You may add fields to the class and initialize them in here. Do not add parameters!
         this.MAX_READERS = maxNumOfReaders;
-        this.accessNumReaderLock = new ReentrantLock();
+        this.numOfReaders = 0;
+        this.isSomeoneWriting = false;
+        this.accessTInfoLock = new ReentrantLock();
+        this.canReadWrite = this.accessTInfoLock.newCondition();
+        this.pidWriter = null;
+        this.pidReaders =  new HashSet<Long>();
     }
 
     public void put(String key, String value) {
@@ -26,33 +37,85 @@ public class Database {
     }
 
     public boolean readTryAcquire() {
-        this.accessNumReaderLock.lock();
-        boolean canRead = this.numOfReaders <= this.MAX_READERS;
-        this.accessNumReaderLock.unlock();
+        this.accessTInfoLock.lock();
+        boolean canRead = (this.numOfReaders < this.MAX_READERS) && (!(this.isSomeoneWriting));
+        if(canRead){
+            this.numOfReaders++;
+            this.pidReaders.add(Thread.currentThread().getId());
+        }
+        this.accessTInfoLock.unlock();
         return canRead;
     }
 
-    public void readAcquire() {
-        this.accessNumReaderLock.lock();
-        this.numOfReaders++;
-        this.accessNumReaderLock.unlock();
-        //this.get();
-        // TODO: Add your code here...
+    public void readAcquire(){
+        try {
+            this.accessTInfoLock.lock();
+            while ((this.numOfReaders >= this.MAX_READERS) || (this.isSomeoneWriting)) {
+                this.canReadWrite.await();
+            }
+            this.numOfReaders++;
+            this.pidReaders.add(Thread.currentThread().getId());
+        }
+        catch (InterruptedException e){
+            //Todo : erase this later
+            throw new RuntimeException("Interrupted exception in read acquire");
+        }
+        finally {
+            this.accessTInfoLock.unlock();
+        }
     }
 
     public void readRelease() {
-        // TODO: Add your code here...
-    }
-
-    public void writeAcquire() {
-       // TODO: Add your code here...
+        this.accessTInfoLock.lock();
+        if(!this.pidReaders.contains(Thread.currentThread().getId())) {
+            this.accessTInfoLock.unlock();
+            throw new IllegalMonitorStateException("Illegal read release attempt");
+        }
+        this.pidReaders.remove(Thread.currentThread().getId());
+        this.numOfReaders--;
+        this.canReadWrite.signal();
+        this.accessTInfoLock.unlock();
     }
 
     public boolean writeTryAcquire() {
-        // TODO: Add your code here...
+        this.accessTInfoLock.lock();
+        boolean canWrite = (this.numOfReaders == 0) && (!(this.isSomeoneWriting));
+        if(canWrite) {
+            this.isSomeoneWriting = true;
+            this.pidWriter = Thread.currentThread().getId();
+        }
+        this.accessTInfoLock.unlock();
+        return canWrite;
+    }
+
+    public void writeAcquire() {
+        // TODO: deal with busy waiting
+        try{
+            this.accessTInfoLock.lock();
+            while ((this.numOfReaders > 0) || (this.isSomeoneWriting)) {
+                this.canReadWrite.await();
+            }
+            this.pidWriter = Thread.currentThread().getId();
+            this.isSomeoneWriting = true;
+        }
+        catch (InterruptedException e){
+            //Todo : erase this later
+            throw new RuntimeException("Interrupted exception in write acquire");
+        }
+        finally {
+            this.accessTInfoLock.unlock();
+        }
     }
 
     public void writeRelease() {
-        // TODO: Add your code here...
+        this.accessTInfoLock.lock();
+        if(this.pidWriter == null || this.pidWriter != Thread.currentThread().getId()){
+            this.accessTInfoLock.unlock();
+            throw new IllegalMonitorStateException("Illegal write release attempt");
+        }
+        this.pidWriter = null;
+        this.isSomeoneWriting = false;
+        this.canReadWrite.signal();
+        this.accessTInfoLock.unlock();
     }
 }
